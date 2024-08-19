8ff75480c17b19c755f7621747091bf477deb94c
Emre Özdin <mrzdnboun@gmail.com>
2024-08-19 20:28:18 +0300

    "Added TextInput components for email and password, and buttons for sign in and create account, with corresponding styles."

--- app/auth/sign-in/index.js
+++ app/auth/sign-in/index.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 import React, { useEffect } from "react";
 import { useNavigation } from "expo-router";
 import Colors from "../../../constants/Colors";
@@ -15,6 +15,7 @@ export default function SignIn() {
       <View
         style={{
           padding: 25,
+          paddingTop: 50,
           backgroundColor: Colors.WHITE,
           height: "100%",
         }}
@@ -23,34 +24,93 @@ export default function SignIn() {
           style={{
             fontFamily: "outfit-bold",
             fontSize: 30,
-            marginTop: 60,
           }}
         >
           Let's Sign You In
         </Text>
         <Text
           style={{
-            fontFamily: "outfit-bold",
+            fontFamily: "outfit-regular",
             fontSize: 30,
-            marginTop: 60,
             color: Colors.GRAY,
-            marginTop: 20,
+            marginTop: 10,
           }}
         >
           Welcome Back
         </Text>
         <Text
           style={{
-            fontFamily: "outfit-bold",
+            fontFamily: "outfit-regular",
             fontSize: 30,
-            marginTop: 60,
             color: Colors.GRAY,
-            marginTop: 20,
           }}
         >
           You've been Missed!
         </Text>
+
+        <View
+          style={{
+            marginTop: 10,
+          }}
+        >
+          <Text style={{ fontFamily: "outfit-regular" }}></Text>
+        </View>
+
+        {/*Email*/}
+        <View>
+          <Text>Email</Text>
+          <TextInput style={styles.input} placeholder="Enter Email" />
+        </View>
+
+        {/*Password*/}
+        <View style={{ marginTop: 20 }}>
+          <Text>Password</Text>
+          <TextInput
+            secureTextEntry={true}
+            style={styles.input}
+            placeholder="Enter Password"
+          />
+        </View>
+
+        {/* SignIn Account Button */}
+        <View
+          style={{
+            padding: 20,
+            backgroundColor: Colors.PRIMARY,
+            borderRadius: 15,
+            marginTop: 50,
+          }}
+        >
+          <Text style={{ color: Colors.WHITE, textAlign: "center" }}>
+            Sign In
+          </Text>
+        </View>
+
+        {/* Create Button */}
+        <View
+          style={{
+            padding: 20,
+            backgroundColor: Colors.WHITE,
+            borderRadius: 15,
+            marginTop: 20,
+            borderWidth: 1,
+          }}
+        >
+          <Text style={{ color: Colors.PRIMARY, textAlign: "center" }}>
+            Create Account
+          </Text>
+        </View>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 15,
+    borderWidth: 1,
+    borderRadius: 15,
+    borderColor: Colors.GRAY,
+    fontFamily: "outfit-regular",
+  },
+});
