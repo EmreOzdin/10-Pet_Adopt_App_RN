2d9d7780ba39f3bc3cc17ca93552e41c183c7bcc
Emre Özdin <mrzdnboun@gmail.com>
2024-08-19 12:18:56 +0300

    loginUI added

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,16 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href={"/login"}>
+        <Text>Go to login screen!</Text>
+      </Link>
     </View>
   );
 }
