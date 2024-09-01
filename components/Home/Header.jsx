acb5d0f8148c1b34f46b699b4d9f99ab2bd80b76
Emre Özdin <mrzdnboun@gmail.com>
2024-09-01 19:55:52 +0300

    "Updated Header component to import Image from react-native, added styles and Image component to display user's profile picture"

--- components/Home/Header.jsx
+++ components/Home/Header.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import { auth } from "../../configs/FirebaseConfig";
 
@@ -6,11 +6,26 @@ export default function Header() {
   const user = auth.currentUser;
 
   return (
-    <View>
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
       <View>
-        <Text>Welcome, </Text>
-        <Text>{user?.metadata}</Text>
+        <Text style={{ fontFamily: "outfit-regular", fontSize: 18 }}>
+          Welcome,
+        </Text>
+        <Text style={{ fontFamily: "outfit-medium", fontSize: 25 }}>
+          {user?.displayName}
+        </Text>
       </View>
+      <Image
+        source={{ uri: user?.photoURL }}
+        style={{ width: 40, height: 40, borderRadius: 99 }}
+      />
     </View>
   );
 }
