741db3cbf431b6a7c9431dcde373bd7c0c220f54
Emre Özdin <mrzdnboun@gmail.com>
2024-09-06 14:39:48 +0300

    "Updated Slider component to fetch data from Firestore and render images in a FlatList"

--- components/Home/Slider.jsx
+++ components/Home/Slider.jsx
@@ -1,23 +1,55 @@
-import { View, Text } from "react-native";
-import React, { useEffect } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "../../configs/FirebaseConfig";
 
 export default function Slider() {
+  const [sliderList, setSliderList] = useState([]);
   // Get data from Firestore
   useEffect(() => {
     GetSlider();
   }, []);
 
   const GetSlider = async () => {
+    setSliderList([]);
     const snapshot = await getDocs(collection(db, "sliders"));
     snapshot.forEach((doc) => {
       console.log(doc.data());
+      setSliderList((sliderList) => [...sliderList, doc.data()]);
     });
   };
   return (
-    <View>
-      <Text>Slider</Text>
+    <View style={{ marginTop: 15 }}>
+      <FlatList
+        data={sliderList}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item, index }) => (
+          <View>
+            {/* Render slider images */}
+            <Image
+              source={{ uri: item?.imageUrl }}
+              style={styles.sliderImage}
+            />
+          </View>
+        )}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  sliderImage: {
+    width: Dimensions.get("screen").width * 0.9,
+    height: 170,
+    borderRadius: 15,
+    marginRight: 15,
+  },
+});
