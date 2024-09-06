72e0d63da2e26b6297056d3386dbf27fde548ce7
Emre Özdin <mrzdnboun@gmail.com>
2024-09-06 14:45:09 +0300

    "Added PetListByCategory component to Home screen and removed empty lines"

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import Header from "../../components/Home/Header";
 import Slider from "../../components/Home/Slider";
+import PetListByCategory from "../../components/Home/PetListByCategory";
 
 export default function Home() {
   return (
@@ -15,9 +16,8 @@ export default function Home() {
       <Header />
       {/* Slider */}
       <Slider />
-
-      {/* Category */}
-
+      {/* Pet List + Category */}
+      <PetListByCategory />
       {/* List of Pets */}
 
       {/* Add New Pet Option */}
---
+++ components/Home/Category.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Category() {
+  return (
+    <View style={{ marginTop: 20 }}>
+      <Text style={{ fontFamily: "outfit-medium", fontSize: 20 }}>
+        Category
+      </Text>
+    </View>
+  );
+}
